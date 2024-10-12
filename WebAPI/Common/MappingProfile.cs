97560b820c37390b62430427391a1df9aad7440c
Berk Akın <berkakin99@hotmail.com>
2024-10-12 17:29:46 +0300

    FEAT: mappings are implemented for movie detail and all movies. Maps are now updated to get genre names instead of all genre information with id field

---
+++ WebAPI/Common/MappingProfile.cs
@@ -0,0 +1,21 @@
+using System.Linq;
+using AutoMapper;
+using WebAPI.Application.MovieOperations.Queries.GetMovieDetail;
+using WebAPI.Application.MovieOperations.Queries.GetMovies;
+using WebAPI.Entites;
+
+namespace WebAPI.Common
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Movie, GetMoviesViewModel>()
+            .ForMember(dest => dest.Genres, opt => opt.MapFrom(src => src.Genres.Select(g => g.Name).ToList()));
+
+            CreateMap<Movie, GetMovieDetailViewModel>()
+            .ForMember(dest => dest.GenreList, opt => opt.MapFrom(src => src.Genres.Select(g => g.Name).ToList()));
+
+        }
+    }
+}
