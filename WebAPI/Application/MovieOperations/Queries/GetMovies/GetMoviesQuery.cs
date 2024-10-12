b2b8d1dc9768a37f2e5f27ff5f1d0579e86e6027
Berk Akın <berkakin99@hotmail.com>
2024-10-12 17:28:44 +0300

    FEAT: Implemented getting all movies query

---
+++ WebAPI/Application/MovieOperations/Queries/GetMovies/GetMoviesQuery.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.DBOperations;
+using WebAPI.Entites;
+using System.Linq;
+
+namespace WebAPI.Application.MovieOperations.Queries.GetMovies
+{
+    public class GetMoviesQuery
+    {
+        private readonly IMapper _mapper;
+        private readonly MovieStoreDbContext _context;
+
+        public GetMoviesQuery(MovieStoreDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        public List<GetMoviesViewModel> Handle()
+        {
+            var movies = _context.Movies.Include(m => m.Genres).ToList();
+            List<GetMoviesViewModel> movieListModel = _mapper.Map<List<GetMoviesViewModel>>(movies);
+            return movieListModel;
+        }
+    }
+
+    public class GetMoviesViewModel
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Year { get; set; }
+
+        public List<string> Genres { get; set; }
+
+    }
+}
