e503c4eaefdf3c21c0b6e573f5f6a2fbfe4dec2f
Berk Akın <berkakin99@hotmail.com>
2024-10-12 17:30:51 +0300

    FEAT: Implemented getMovies and getMovieDetail endpoints

---
+++ WebAPI/Controllers/MovieController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Application.MovieOperations.Queries.GetMovieDetail;
+using WebAPI.Application.MovieOperations.Queries.GetMovies;
+using WebAPI.DBOperations;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+
+
+    public class MovieController : ControllerBase
+    {
+        private readonly MovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public MovieController(MovieStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            GetMoviesQuery query = new GetMoviesQuery(_context, _mapper);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovieDetail(int id)
+        {
+            GetMovieDetailQuery query = new GetMovieDetailQuery(_context, _mapper);
+            query.MovieId = id;
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+    }
+}
