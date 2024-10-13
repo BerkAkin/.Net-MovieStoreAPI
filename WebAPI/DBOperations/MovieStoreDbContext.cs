bd16fe265ec5e32589a299c78153a22e594673ab
Berk Akın <berkakin99@hotmail.com>
2024-10-14 00:09:46 +0300

    FEAT: Implemented Movie entity's producer are as OneToMany relation to add producer info to ouput json

--- WebAPI/DBOperations/MovieStoreDbContext.cs
+++ WebAPI/DBOperations/MovieStoreDbContext.cs
@@ -21,11 +21,16 @@ namespace WebAPI.DBOperations
                 .HasMany(m => m.Actors)
                 .WithMany(a => a.Movies);
 
+            modelBuilder.Entity<Movie>()
+                .HasOne(p => p.Producer)
+                .WithMany(m => m.Movies)
+                .HasForeignKey(m => m.ProducerId);
+
         }
 
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Genre> Genres { get; set; }
-
         public DbSet<Actor> Actors { get; set; }
+        public DbSet<Producer> Producers { get; set; }
     }
 }
