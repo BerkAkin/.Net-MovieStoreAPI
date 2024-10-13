49ef3109a7f42e5c95669cc450d87860b946822a
Berk Akın <berkakin99@hotmail.com>
2024-10-13 13:47:50 +0300

    FEAT: Implemented Delete Command

---
+++ WebAPI/Application/MovieOperations/Commands/DeleteMovie/DeleteMovieCommand.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using AutoMapper;
+using WebAPI.DBOperations;
+
+namespace WebAPI.Application.MovieOperations.Commands.DeleteMovie
+{
+    public class DeleteMovieCommand
+    {
+        private readonly MovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public int Id { get; set; }
+        public DeleteMovieCommand(MovieStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var movie = _context.Movies.SingleOrDefault(x => x.Id == Id);
+            if (movie is null)
+            {
+                throw new InvalidOperationException("Silinecek Film Bulunamadı");
+            }
+            else
+            {
+                _context.Movies.Remove(movie);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
