0a47cae6d9bfd3fb27641c1836acfc7c66afc093
Berk Akın <berkakin99@hotmail.com>
2024-10-13 13:35:39 +0300

    FEAT: Implemented mapping options for movie and related genres to it

--- WebAPI/Common/MappingProfile.cs
+++ WebAPI/Common/MappingProfile.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using AutoMapper;
+using WebAPI.Application.MovieOperations.Commands.CreateMovie;
 using WebAPI.Application.MovieOperations.Queries.GetMovieDetail;
 using WebAPI.Application.MovieOperations.Queries.GetMovies;
 using WebAPI.Entites;
@@ -15,6 +16,9 @@ namespace WebAPI.Common
 
             CreateMap<Movie, GetMovieDetailViewModel>()
             .ForMember(dest => dest.GenreList, opt => opt.MapFrom(src => src.Genres.Select(g => g.Name).ToList()));
+
+            CreateMap<CreateMovieViewModel, Movie>().ForMember(dest => dest.Genres, opt => opt.Ignore());
+            CreateMap<string, Genre>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src));
 
         }
     }
