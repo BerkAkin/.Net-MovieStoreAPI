617927cb40be82b184843f8ee782a2c5d2c7ea75
Berk Akın <berkakin99@hotmail.com>
2024-10-13 23:37:08 +0300

    FEAT: DB is now populating with actors too

--- WebAPI/DBOperations/DbSeeder.cs
+++ WebAPI/DBOperations/DbSeeder.cs
@@ -18,33 +18,39 @@ namespace WebAPI.DBOperations
                     return;
                 }
 
-                var actionGenre = new Genre { Name = "Action" };
-                var comedyGenre = new Genre { Name = "Comedy" };
-                var horrorGenre = new Genre { Name = "Horror" };
-                var romanticGenre = new Genre { Name = "Romantic" };
-
-                context.Genres.AddRange(actionGenre, comedyGenre);
+                Genre actionGenre = new Genre() { Name = "Action" };
+                Genre comedyGenre = new Genre() { Name = "Comedy" };
+                Genre horrorGenre = new Genre() { Name = "Horror" };
+                Genre romanticGenre = new Genre() { Name = "Romantic" };
+                context.Genres.AddRange(actionGenre, comedyGenre, horrorGenre, romanticGenre);
                 context.SaveChanges();
 
 
-                var movie = new Movie
-                {
-                    Title = "Action Comedy Movie",
-                    Price = 123,
-                    Year = DateTime.Now,
-                    Genres = new List<Genre> { actionGenre, comedyGenre }
-                };
-
-                var movie2 = new Movie
-                {
-                    Title = "Horror Romantic Movie",
-                    Price = 123,
-                    Year = DateTime.Now,
-                    Genres = new List<Genre> { horrorGenre, romanticGenre }
-                };
+                Actor hugh = new Actor() { Name = "Hugh", Surname = "Jackman" };
+                Actor jfer = new Actor() { Name = "Jennifer", Surname = "Lawrence" };
+                context.Actors.AddRange(hugh, jfer);
+                context.SaveChanges();
 
 
-                context.Movies.AddRange(movie, movie2);
+                context.Movies.AddRange(
+                    new Movie
+                    {
+                        Title = "Action Comedy Movie",
+                        Price = 123,
+                        Year = DateTime.Now,
+                        Genres = new List<Genre> { actionGenre, comedyGenre },
+                        Actors = new List<Actor> { hugh }
+                    },
+                    new Movie
+                    {
+                        Title = "Horror Romantic Movie",
+                        Price = 123,
+                        Year = DateTime.Now,
+                        Genres = new List<Genre> { horrorGenre, romanticGenre },
+                        Actors = new List<Actor> { jfer }
+                    }
+                );
+
                 context.SaveChanges();
             }
         }
