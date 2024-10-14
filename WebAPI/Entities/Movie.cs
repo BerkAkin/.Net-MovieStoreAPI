bf83e67e87ad20fc144a113a35b9dc2eced7afb0
Berk Akın <berkakin99@hotmail.com>
2024-10-14 19:35:44 +0300

    FIX: movies on genre entity can be left empty now

--- WebAPI/Entities/Movie.cs
+++ WebAPI/Entities/Movie.cs
@@ -11,7 +11,7 @@ namespace WebAPI.Entites
         public string Title { get; set; }
         public decimal Price { get; set; }
         public DateTime Year { get; set; }
-        public ICollection<Genre> Genres { get; set; }
+        public ICollection<Genre> Genres { get; set; } = new List<Genre>();
         public ICollection<Actor> Actors { get; set; }
         public ICollection<Customer> Customers { get; set; }
         public int ProducerId { get; set; }
