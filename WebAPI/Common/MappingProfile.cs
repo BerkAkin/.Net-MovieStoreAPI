60d9bd9754f516ef319f3073990209385f0bb6a3
Berk Akın <berkakin99@hotmail.com>
2024-10-14 19:34:44 +0300

    FIX: Producer id can now be ignored

--- WebAPI/Common/MappingProfile.cs
+++ WebAPI/Common/MappingProfile.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using AutoMapper;
+using WebAPI.Application.GenreOperations.Commands.CreateGenre;
 using WebAPI.Application.GenreOperations.Queries.GetGenreDetail;
 using WebAPI.Application.GenreOperations.Queries.GetGenres;
 using WebAPI.Application.MovieOperations.Commands.CreateMovie;
@@ -25,7 +26,10 @@ namespace WebAPI.Common
             .ForMember(dest => dest.Actors, opt => opt.MapFrom(src => src.Actors.Select(g => g.Name + " " + g.Surname).ToList()))
             .ForMember(dest => dest.Producer, opt => opt.MapFrom(src => src.Producer.Name + " " + src.Producer.Surname));
 
-            CreateMap<CreateMovieViewModel, Movie>().ForMember(dest => dest.Genres, opt => opt.Ignore());
+            CreateMap<CreateMovieViewModel, Movie>().ForMember(dest => dest.Genres, opt => opt.Ignore())
+            .ForMember(dest => dest.Producer, opt => opt.Ignore()).ForMember(dest => dest.ProducerId, opt => opt.Ignore());
+            ;
+
             CreateMap<string, Genre>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src));
 
 
@@ -35,6 +39,8 @@ namespace WebAPI.Common
 
             CreateMap<Genre, GetGenreDetailViewModel>()
             .ForMember(dest => dest.Movies, opt => opt.MapFrom(src => src.Movies.Select(m => m.Title).ToList()));
+
+            CreateMap<CreateGenreViewModel, Genre>();
 
         }
     }
