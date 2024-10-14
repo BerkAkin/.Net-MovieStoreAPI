78b9532db6bcdc119423faf979d8657315f20d17
Berk Akın <berkakin99@hotmail.com>
2024-10-14 19:34:15 +0300

    FIX: producer's id is now fetching from producers table

--- WebAPI/Application/MovieOperations/Commands/CreateMovie/CreatMovieCommand.cs
+++ WebAPI/Application/MovieOperations/Commands/CreateMovie/CreatMovieCommand.cs
@@ -24,22 +24,34 @@ namespace WebAPI.Application.MovieOperations.Commands.CreateMovie
         public void Handle()
         {
             var movie = _context.Movies.SingleOrDefault(m => m.Title == Model.Title);
+
             if (movie is not null)
             {
                 throw new InvalidOperationException("Eklemeye Çalıştığınız Film Zaten Mevcut. Lütfen Başka Bir Film Başlığı Deneyin");
             }
-            else
+
+
+            movie = _mapper.Map<Movie>(Model);
+
+            var genres = _context.Genres.Where(g => Model.Genres.Contains(g.Name)).ToList();
+
+
+            var producer = _context.Producers.Where(p => p.Id == Model.ProducerId).SingleOrDefault();
+            /*string[] FullName = new string[2];
+              FullName = Model.Producer.Split(" ");
+            AD SOYAD BİLGİSİNDEN PRODUCER ID BULUNMASI VE ATAMA YAPILMASI. FRONT-END TARAFINDA BÖYLE OLACAK MANTIKEN
+            var producer = _context.Producers.SingleOrDefault(p => p.Name == FullName[0] && p.Surname == FullName[1]); */
+
+            movie.Genres = genres;
+            movie.ProducerId = producer.Id;
+
+            if (movie.Genres == null || !movie.Genres.Any())
             {
-                movie = _mapper.Map<Movie>(Model);
-                var genres = _context.Genres.Where(g => Model.Genres.Contains(g.Name)).ToList();
-                if (!genres.Any())
-                {
-                    throw new InvalidOperationException("Geçersiz genre isimleri.");
-                }
-                movie.Genres = genres;
-                _context.Add(movie);
-                _context.SaveChanges();
+                throw new InvalidOperationException("Bir filmin en az bir türü olmalıdır");
             }
+            _context.Add(movie);
+            _context.SaveChanges();
+
         }
     }
 
@@ -49,5 +61,7 @@ namespace WebAPI.Application.MovieOperations.Commands.CreateMovie
         public decimal Price { get; set; }
         public DateTime Year { get; set; }
         public List<string> Genres { get; set; }
+        public int ProducerId { get; set; }
+
     }
 }
