ee671345030f044432850bf99b1012daaf6d5e76
Berk Akın <berkakin99@hotmail.com>
2024-10-14 15:45:50 +0300

    FEAT: Added genre mapping options

--- WebAPI/Common/MappingProfile.cs
+++ WebAPI/Common/MappingProfile.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using AutoMapper;
+using WebAPI.Application.GenreOperations.Queries.GetGenres;
 using WebAPI.Application.MovieOperations.Commands.CreateMovie;
 using WebAPI.Application.MovieOperations.Queries.GetMovieDetail;
 using WebAPI.Application.MovieOperations.Queries.GetMovies;
@@ -11,6 +12,8 @@ namespace WebAPI.Common
     {
         public MappingProfile()
         {
+
+            //MOVIE MAPPING SETTINGS
             CreateMap<Movie, GetMoviesViewModel>()
             .ForMember(dest => dest.Genres, opt => opt.MapFrom(src => src.Genres.Select(g => g.Name).ToList()))
             .ForMember(dest => dest.Actors, opt => opt.MapFrom(src => src.Actors.Select(g => g.Name + " " + g.Surname).ToList()))
@@ -21,10 +24,13 @@ namespace WebAPI.Common
             .ForMember(dest => dest.Actors, opt => opt.MapFrom(src => src.Actors.Select(g => g.Name + " " + g.Surname).ToList()))
             .ForMember(dest => dest.Producer, opt => opt.MapFrom(src => src.Producer.Name + " " + src.Producer.Surname));
 
-
-
             CreateMap<CreateMovieViewModel, Movie>().ForMember(dest => dest.Genres, opt => opt.Ignore());
             CreateMap<string, Genre>().ForMember(dest => dest.Name, opt => opt.MapFrom(src => src));
+
+
+            //GENRE MAPPING SETTINGS
+            CreateMap<Genre, GetGenresViewModel>()
+            .ForMember(dest => dest.Movies, opt => opt.MapFrom(src => src.Movies.Select(m => m.Title).ToList()));
 
         }
     }
