dba97949f3ec9a6501b576028cdce76039845410
Berk Akın <berkakin99@hotmail.com>
2024-10-15 14:21:46 +0300

    FEAT: Update Actor Command implemented

---
+++ WebAPI/Application/ActorOperations/Commands/UpdateActor/UpdateActorCommand.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using AutoMapper;
+using WebAPI.DBOperations;
+
+namespace WebAPI.Application.ActorOperations.Commands.UpdateActor
+{
+    public class UpdateActorCommand
+    {
+        private readonly MovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public int ActorId { get; set; }
+        public UpdateActorViewModel Model { get; set; }
+        public UpdateActorCommand(MovieStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public void Handle()
+        {
+            var actor = _context.Actors.SingleOrDefault(g => g.Id == ActorId);
+            if (actor is null)
+            {
+                throw new InvalidOperationException("Güncellenecek tür bulunamadı");
+            }
+            actor.Name = Model.Name != default ? Model.Name : actor.Name;
+            actor.Surname = Model.Surname != default ? Model.Surname : actor.Surname;
+            _context.SaveChanges();
+        }
+    }
+
+    public class UpdateActorViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
