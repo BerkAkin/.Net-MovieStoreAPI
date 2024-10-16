df15ec5ee1ac14fff157a984b389668be49f0b7b
Berk Akın <berkakin99@hotmail.com>
2024-10-16 20:59:29 +0300

    FEAT: Producer detail query implemented

---
+++ WebAPI/Application/ProducerOperations/Queries/GetProducerDetail/GetProducerDetail.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DBOperations;
+using WebAPI.Entites;
+
+namespace WebAPI.Application.ProdcuerOperations.Queries.GetProducerDetail
+{
+    public class GetProducerDetailQuery
+    {
+        private readonly MovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public int Id { get; set; }
+        public GetProducerDetailQuery(MovieStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public GetProducerDetailViewModel Handle()
+        {
+            var producer = _context.Producers.Include(m => m.Movies).SingleOrDefault(p => p.Id == Id);
+            if (producer is null)
+            {
+                throw new InvalidOperationException("Yapımcı bulunamadı");
+
+            }
+            GetProducerDetailViewModel vm = _mapper.Map<GetProducerDetailViewModel>(producer);
+            return vm;
+        }
+    }
+
+    public class GetProducerDetailViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public ICollection<string> Movies { get; set; }
+    }
+}
