ab1eb2c80830ba1d86b136043e6c6d6d97a821be
Berk Akın <berkakin99@hotmail.com>
2024-10-16 20:59:36 +0300

    FEAT: Producers query implemented

---
+++ WebAPI/Application/ProducerOperations/Queries/GetProducers/GetProducersQuery.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DBOperations;
+using WebAPI.Entites;
+
+namespace WebAPI.Application.ProdcuerOperations.Queries.GetProducers
+{
+    public class GetProducersQuery
+    {
+        private readonly MovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public GetProducersQuery(MovieStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public List<GetProducerViewModel> Handle()
+        {
+            List<Producer> producers = _context.Producers.Include(m => m.Movies).ToList();
+            if (producers is null)
+            {
+                throw new InvalidOperationException("Yapımcı Yok");
+            }
+            List<GetProducerViewModel> vm = _mapper.Map<List<GetProducerViewModel>>(producers);
+            return vm;
+        }
+    }
+    public class GetProducerViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public ICollection<string> Movies { get; set; }
+    }
+}
