2d01dc349b09c1d1232207d43f2f0c950a6ee0f3
Berk Akın <berkakin99@hotmail.com>
2024-10-16 21:00:43 +0300

    FEAT: Producer controller implemented

---
+++ WebAPI/Controllers/ProducerController.cs
@@ -0,0 +1,77 @@
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Application.ProdcuerOperations.Commands.CreateProducer;
+using WebAPI.Application.ProdcuerOperations.Commands.DeleteProducer;
+using WebAPI.Application.ProdcuerOperations.Commands.UpdateProducer;
+using WebAPI.Application.ProdcuerOperations.Queries.GetProducerDetail;
+using WebAPI.Application.ProdcuerOperations.Queries.GetProducers;
+using WebAPI.DBOperations;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+
+
+    public class ProducerController : ControllerBase
+    {
+        private readonly MovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ProducerController(MovieStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult getProducers()
+        {
+            GetProducersQuery query = new GetProducersQuery(_context, _mapper);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getProducerDetail(int id)
+        {
+            GetProducerDetailQuery query = new GetProducerDetailQuery(_context, _mapper);
+            query.Id = id;
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProducer([FromBody] CreateProducerViewModel model)
+        {
+            CreateProducerCommand command = new CreateProducerCommand(_context, _mapper);
+            command.Model = model;
+            command.Handle();
+            return Ok("Yapımcı oluşturma başarılı");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProducer(int id)
+        {
+            DeleteProducerCommand command = new DeleteProducerCommand(_context, _mapper);
+            command.Id = id;
+            command.Handle();
+            return Ok("Silme başarılı");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProducer(int id, UpdateProducerViewModel model)
+        {
+            UpdateProducerCommand command = new UpdateProducerCommand(_context, _mapper);
+            command.Model = model;
+            command.Id = id;
+            command.Handle();
+            return Ok("Güncelleme Başarılı");
+        }
+
+
+
+
+    }
+}
