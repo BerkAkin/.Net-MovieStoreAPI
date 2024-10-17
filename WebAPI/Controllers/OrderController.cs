5ad671b8a43ebbed568d00ff6bb30ce47baea8b8
Berk Akın <berkakin99@hotmail.com>
2024-10-17 19:59:03 +0300

    FEAT: Order endpoints defined

---
+++ WebAPI/Controllers/OrderController.cs
@@ -0,0 +1,94 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Application.OrderOperations.Commands.CreateOrder;
+using WebAPI.Application.OrderOperations.Commands.DeleteOrder;
+using WebAPI.Application.OrderOperations.Commands.SetOrderAtatusActive;
+using WebAPI.Application.OrderOperations.Commands.UpdateOrder;
+using WebAPI.Application.OrderOperations.Queries.GetInactiveOrders;
+using WebAPI.Application.OrderOperations.Queries.GetOrders;
+using WebAPI.DBOperations;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+
+
+    public class OrderController : ControllerBase
+    {
+        private readonly MovieStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public OrderController(MovieStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            GetOrdersQuery query = new GetOrdersQuery(_context, _mapper);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("Inactive")]
+        public IActionResult GetInactiveOrders()
+        {
+            GetInactiveOrdersQuery query = new GetInactiveOrdersQuery(_context, _mapper);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+
+        [HttpGet("{CustomerId}")]
+        public IActionResult GetMovieDetail(int CustomerId)
+        {
+            GetOrderDetailQuery query = new GetOrderDetailQuery(_context, _mapper);
+            query.CustomerId = CustomerId;
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie([FromBody] CreateOrderViewModel order)
+        {
+            CreateOrderCommand command = new CreateOrderCommand(_context, _mapper);
+            command.Model = order;
+            command.Handle();
+            return Ok("Sipariş Ekleme Başarılı !");
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(int id)
+        {
+            DeleteOrderCommand command = new DeleteOrderCommand(_context, _mapper);
+            command.OrderId = id;
+            command.Handle();
+            return Ok("Silme başarılı");
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateOrder(int id, UpdateOrderViewModel model)
+        {
+            UpdateOrderCommand command = new UpdateOrderCommand(_context, _mapper);
+            command.Model = model;
+            command.OrderId = id;
+            command.Handle();
+            return Ok("Güncelleme Başarılı");
+        }
+
+        [HttpPut("{id}/ChangeStatus")]
+        public IActionResult UpdateGenre(int id)
+        {
+            SetOrderStatusActiveCommand command = new SetOrderStatusActiveCommand(_context, _mapper);
+            command.OrderId = id;
+            command.Handle();
+            return Ok("Durum Değişimi Başarılı");
+        }
+
+    }
+}
